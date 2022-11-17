92746435e4b5c30df05df7b9df663bc78c6b8bb4
Gurvinder Singh Yadav <105541779+gurvinder-singh-yadav@users.noreply.github.com>
2022-11-17 16:51:24 +0530

    Firebase Connection with firebase MessagingService and FirebaseDatabse

--- app/src/main/java/com/example/chatapp/activities/SignInActivity.java
+++ app/src/main/java/com/example/chatapp/activities/SignInActivity.java
@@ -5,9 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.chatapp.R;
 import com.example.chatapp.databinding.ActivitySignInBinding;
+import com.google.common.base.MoreObjects;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
     private ActivitySignInBinding binding;
@@ -23,6 +28,20 @@ public class SignInActivity extends AppCompatActivity {
     private void setListener(){
         binding.textCreateNewAccount.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+                binding.buttonSignIn.setOnClickListener(v -> addDataToFirestore());
     }
-
+    private void addDataToFirestore(){
+        FirebaseFirestore db  = FirebaseFirestore.getInstance();
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("first_name", "Gurvinder");
+        data.put("last_name", "Yadav");
+        db.collection("users")
+                .add(data)
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(this, "Data Inserted succesfully", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(exception ->{
+                    Toast.makeText(this, "Data Could not be inserted", Toast.LENGTH_SHORT).show();
+                });
+    }
 }
