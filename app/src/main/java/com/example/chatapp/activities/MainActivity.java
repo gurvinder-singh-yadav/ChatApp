377d26d6ae30604d63fe3a9ecf7f59a8b7b0a200
Gurvinder Singh Yadav <105541779+gurvinder-singh-yadav@users.noreply.github.com>
2022-11-17 18:28:06 +0530

    SignUP page backend: firebase connection, shared PreferenceManager

--- app/src/main/java/com/example/chatapp/activities/MainActivity.java
+++ app/src/main/java/com/example/chatapp/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.chatapp;
+package com.example.chatapp.activities;
+
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.chatapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
--- app/src/main/java/com/example/chatapp/activities/SignInActivity.java
+++ app/src/main/java/com/example/chatapp/activities/SignInActivity.java
@@ -1,21 +1,20 @@
 package com.example.chatapp.activities;
 
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.widget.Toast;
 
-import com.example.chatapp.R;
-import com.example.chatapp.databinding.ActivitySignInBinding;
-import com.google.common.base.MoreObjects;
-import com.google.firebase.firestore.FirebaseFirestore;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.HashMap;
+import com.example.chatapp.databinding.ActivitySignInBinding;
+
+import java.util.regex.Pattern;
 
 public class SignInActivity extends AppCompatActivity {
     private ActivitySignInBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,20 +27,8 @@ public class SignInActivity extends AppCompatActivity {
     private void setListener(){
         binding.textCreateNewAccount.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
-                binding.buttonSignIn.setOnClickListener(v -> addDataToFirestore());
+
     }
-    private void addDataToFirestore(){
-        FirebaseFirestore db  = FirebaseFirestore.getInstance();
-        HashMap<String, Object> data = new HashMap<>();
-        data.put("first_name", "Gurvinder");
-        data.put("last_name", "Yadav");
-        db.collection("users")
-                .add(data)
-                .addOnSuccessListener(documentReference -> {
-                    Toast.makeText(this, "Data Inserted succesfully", Toast.LENGTH_SHORT).show();
-                })
-                .addOnFailureListener(exception ->{
-                    Toast.makeText(this, "Data Could not be inserted", Toast.LENGTH_SHORT).show();
-                });
-    }
+
+
 }
